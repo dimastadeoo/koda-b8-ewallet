1785a50ba4403df442ccad2fc4892ad6b3fe25db
dimastadeoo <dimastadeoo@gmail.com>
2026-07-16 11:48:37 +0700

    feat: Remove id_user column and correct status value in sessions table migration

--- migrations/000003_create_sessions_table.up.sql
+++ migrations/000003_create_sessions_table.up.sql
@@ -1,8 +1,7 @@
 CREATE TABLE "sessions"(
     "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "id_user" BIGINT REFERENCES "users"("id") NOT NULL, 
     "token" VARCHAR(100) UNIQUE,
-    "status" VARCHAR(30) NOT NULL CHECK ("status" IN ('login', 'logout', 'register','forgot-password')),
+    "status" VARCHAR(30) NOT NULL CHECK ("status" IN ('login', 'logout', 'register','forgot-pin')),
     "created_at" TIMESTAMP DEFAULT NOW(),
     "updated_at" TIMESTAMP DEFAULT NOW()
 );
