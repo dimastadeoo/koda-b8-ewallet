8a3e43cbc51f866b141f0560ebccece18fe31fdd
dimastadeoo <dimastadeoo@gmail.com>
2026-07-16 15:19:09 +0700

    fix: Correct SQL syntax in users_logs and wallet table migrations

--- migrations/000004_create_users_logs_table.up.sql
+++ migrations/000004_create_users_logs_table.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE "users_logs"(
     "id_sesion" BIGINT REFERENCES "sessions"("id") NOT NULL, 
     "activity_detail" TEXT NOT NULL,
     "ip_address" VARCHAR(80),
-    "created_at" TIMESTAMP DEFAULT NOW(),
+    "created_at" TIMESTAMP DEFAULT NOW()
 );
--- migrations/000006_create_wallet_table.up.sql
+++ migrations/000006_create_wallet_table.up.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "wallets" {
+CREATE TABLE "wallets" (
     "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "id_user" BIGINT REFERENCES "users"("id") NOT NULL,
     "balance" NUMERIC(18,2) NOT NULL DEFAULT 0,
     "currency" VARCHAR(3) NOT NULL DEFAULT 'IDR',
-    "status" VARCHAR(20) NOT NULL DEFAULT 'active'
+    "status_account" VARCHAR(20) NOT NULL DEFAULT 'active'
         CHECK ("status_account" IN ('active','suspended','blocked')),
     "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
-};
+);
