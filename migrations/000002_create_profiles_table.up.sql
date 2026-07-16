0bc19d1268d7f412ea60877264cb160e201f7033
dimastadeoo <dimastadeoo@gmail.com>
2026-07-16 11:07:50 +0700

    feat: Add migration files to create profiles table

---
+++ migrations/000002_create_profiles_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "profiles" (
+    "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "id_user" BIGINT REFERENCES "users"("id") NULL,
+    "nik" VARCHAR(16) UNIQUE,
+    "name" VARCHAR(150) NOT NULL,
+    "address" TEXT,
+    "gender" VARCHAR(1) CHECK ("gender" IN ('M', 'F')) NOT NULL,
+    "place_birth" VARCHAR(100),
+    "date_birth" DATE,
+    "created_at" TIMESTAMP DEFAULT NOW(),
+    "updated_at" TIMESTAMP DEFAULT NOW()
+);
+
+CREATE UNIQUE INDEX unique_nik_idx ON "profiles"("nik") WHERE "nik" IS NOT NULL;
