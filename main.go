23c1f78decadb8522e9c97e2eda21e55e86a4bdd
dimastadeoo <dimastadeoo@gmail.com>
2026-07-16 14:54:55 +0700

    refactor: Clean up main function and organize imports

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
+import (
+	"context"
 
+	"github.com/dimastadeoo/koda-b8-ewallet/internal/lib"
+	"github.com/dimastadeoo/koda-b8-ewallet/internal/menu"
+	"github.com/dimastadeoo/koda-b8-ewallet/internal/service"
+	
+)
+
+func main() {
+
+	conn := lib.Conn()
+	defer conn.Close(context.Background())
+
+	authService := service.NewAuthService(conn)
+
+	home := menu.NewHomeMenu(authService)
+
+	home.Home()
+}
